83dd31d40602d9d87672f17c7a37382ded9485ea
jsb9009 <jaliya.bandara1992@gmail.com>
2017-07-06 10:32:24 +0530

    initial commit of getdate

---
+++ src/example/Test.java
@@ -0,0 +1,69 @@
+package example;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Created by jaliya on 7/5/17.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Insert first date : ");
+        String a = s.nextLine();
+        System.out.print("Insert second date : ");
+        String b = s.nextLine();
+
+            /* DateTimeFormatter formatter = DateTimeFormatter.forPattern("dd/MM/yyyy").withLocale(Locale.UK);
+
+
+            LocalDate date = formatter.parseLocalDate(b);
+
+            System.out.println(date.getYear());
+            System.out.println(date.getMonthOfYear());
+            System.out.println(date.getDayOfMonth());
+
+                                                       */
+
+
+        String dateParts1[] = a.split("-");
+        String day1  = dateParts1[0];
+        String month1  = dateParts1[1];
+        String year1 = dateParts1[2];
+
+        String dateParts2[] = b.split("-");
+        String day2  = dateParts2[0];
+        String month2  = dateParts2[1];
+        String year2 = dateParts2[2];
+
+
+        int result1 = Integer.parseInt(day1);
+        int result2 = Integer.parseInt(month1);
+        int result3 = Integer.parseInt(year1);
+
+        int result4 = Integer.parseInt(day2);
+        int result5 = Integer.parseInt(month2);
+        int result6 = Integer.parseInt(year2);
+
+        LocalDate fromDay = LocalDate.of(result3,result2,result1);
+        LocalDate toDay = LocalDate.of(result6,result5,result4);
+
+        Period age = Period.between(fromDay,toDay);
+        int years = age.getYears();
+        int months = age.getMonths();
+        int days = age.getDays();
+
+
+        System.out.println("number of years: " + years);
+        System.out.println("number of months: " + months);
+        System.out.println("number of months: " + days);
+    }
+
+}
